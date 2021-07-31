4ac05f8c5931bdce1886216a8363255df761afda
Nick Sosinski <Nick.j.sosinski@gmail.com>
2021-07-31 15:13:27 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME-HN2RPP
-// @version      2021.7.31.2
+// @version      2021.7.31.3
 // @description  Converts HouseNumbers to RPPs
 // @author       njs923/nicknick923
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
@@ -88,8 +88,8 @@
         let texts = {
             makeRPPButtonText: 'HN→RPP (EP on HN)',
             makeStreetSideRPPButtonText: 'HN→RPP (EP on street)',
-            makeRPPTitleText: 'Creates RPPs where the EP is at the old HN location',
-            makeStreetSideRPPTitleText: 'Creates RPPs where the EP is at the street side',
+            makeRPPTitleText: 'Creates RPPs where the entry point (EP) is at the old HN location',
+            makeStreetSideRPPTitleText: 'Creates RPPs with entry point (EP) at HN entry point',
             noDuplicatesLabel: 'No RPP duplicates',
             delHNButtonText: "Delete HN",
             defaultLockLevel: 'Default lock level',
